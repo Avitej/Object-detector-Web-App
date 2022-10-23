eb3915a3c7bf2700bec93a4917f7c72d8e8976cb
Avitej <82682098+Avitej@users.noreply.github.com>
2022-10-23 15:09:04 -0500

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,23 @@
+status = "";
+img = "";
+function preload() {
+    img = loadImage('bottle.jpg')
+}
+function setup() {
+    canvas = createCanvas(620, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded (){
+    console.log("model loaded");
+    status= true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult (error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
